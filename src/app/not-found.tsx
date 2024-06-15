246fa01ad90b02519ab67fc168bb90d206a3a4bb
Ryo Hilmi <ryodm.74@gmail.com>
2024-06-15 10:38:07 +0700

    feat: add 404 img

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -2,13 +2,14 @@
 
 import { Button } from "@/components/ui/button";
 import Link from "next/link";
+import Image from "next/image";
 import React from "react";
 
 export default function notFound() {
   return (
     <main className="z-20 bg-white w-full h-screen fixed top-0 left-0 flex justify-center items-center">
-      <div className="flex flex-col gap-6">
-        <p className="text-3xl relative -top-4">Page not found :(</p>
+      <div className="flex flex-col gap-6 items-center">
+        <Image src="/404.png" alt="404" width={600} height={400} />
 
         <Link href="/">
           <Button>Go to Home</Button>
